4d580d70d192f9d3dd3ff73ff753659b36e1b9e4
matt-carmo <matheus_2018@hotmail.com>
2022-10-21 14:25:06 -0300

    validate-inputs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+(() => {
+  'use strict';
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation');
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach((form) => {
+    form.addEventListener('submit', (event) => {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    }, false);
+  });
+})();
+
+
+
 const eyeIcon = document.querySelector('.eye-icon')
 const inputPassword = document.querySelector('input[type="password"]')
 console.log(inputPassword)
@@ -7,3 +27,5 @@ eyeIcon.addEventListener('click', () => {
   eyeIcon.classList.toggle('fa-eye-slash')
   inputPassword.type == 'password' ? inputPassword.type = 'text' :  inputPassword.type = 'password';
 })
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
