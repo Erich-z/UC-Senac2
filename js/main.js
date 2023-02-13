c338f6b5cad05ee251d7c9a6ae2baed3734f0232
MATHEUS HENRIQUE GONZAGA DO CARMO <matheus.hgcarmo@senacsp.edu.br>
2023-02-13 19:52:07 -0300

    check-only

--- js/main.js
+++ js/main.js
@@ -16,8 +16,39 @@
   });
 })();
 
+const fileUploadImage = document.querySelector('#file-upload-photo-profile')
+const imgUser = document.querySelector('.img-user');
+
+const inputTxtCPF = document.querySelector("[name=txtCPF]")
+
+const checkNull = document.querySelectorAll(".check-null")
+
+const  includeReadOnly =  (input) => {
+  input.value.length > 0 ? input.readOnly = true : ''
+}
+checkNull.forEach(input => includeReadOnly(input))
 
 
+
+includeReadOnly(inputTxtCPF)
+
+
+fileUploadImage.addEventListener('change', (event) => {
+  const reader = new FileReader();
+  // Define o que ocorre quando concluir:
+  reader.onload = function() {
+     // Define o `src` do elemento para o resultado:
+
+     console.log(reader.result)
+     imgUser.src = reader.result;
+  }
+  // Lê o arquivo e cria um link (o resultado vai ser enviado para o onload.
+
+  console.log(event.target.files[0])
+  reader.readAsDataURL(event.target.files[0]);
+
+
+})
 const eyeIcon = document.querySelector('.eye-icon')
 const inputPassword = document.querySelectorAll('input[type="password"]')
 console.log(inputPassword)
