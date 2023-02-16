2a6672465febdc7b4a4f4dd1d063d556e4ecd319
erich-z <erichrx0703@gmail.com>
2023-02-15 22:13:35 -0300

    fodase

--- pages/user-profile.php
+++ pages/user-profile.php
@@ -107,10 +107,31 @@ $perfilUsuario = $perfilselecionad->fetch(PDO::FETCH_OBJ);
                         
                         
                         <a href="#"></a> 
-                        <button type="button" class="btn btn-primary btn-floating"><i class="fa-solid fa-pen-to-square"></i></button>
+                        <button type="button" class="btn btn-primary btn-floating" ><i class="fa-solid fa-pen-to-square"></i></button>
                       
-                        <button type="button" class="btn btn-primary btn-floating"><i class="fa-solid fa-trash"></i></button>
-                        
+                        <button type="button" class="btn btn-primary btn-floating" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-trash"></i></button>
+                     
+                    <?php 
+                    
+                    ?>
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Deseja deletar esse anuncio?</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
                             
                     </div>
                     <p class="card-text text-secondary">
