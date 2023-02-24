25d3199f207f536cb66876c7cb0c854212ea1640
Vinicus Soares <vinicius_pereirasoares@hotmail.com>
2023-02-24 01:35:28 -0300

    edithouse-quase-ok

--- pages/edithouse.php
+++ pages/edithouse.php
@@ -129,51 +129,7 @@ $linhaEdit = $sqlEdita->fetch(PDO::FETCH_OBJ);
           </div>
         </div>
   
-        <div class="col-lg-4 mb-4 mb-lg-0">
-          <div
-            class="bg-image hover-overlay ripple shadow-1-strong rounded"
-            data-ripple-color="light"
-          >
-            <img
-              src="../img/img2.jpg"
-              class="w-100"
-            />
-            <a href="#!" data-mdb-toggle="modal" data-mdb-target="#exampleModal2">
-              <div class="mask" style="background-color: rgba(251, 251, 251, 0.2);"></div>
-            </a>
-          </div>
-        </div>
-  
-        <div class="col-lg-4 mb-4 mb-lg-0">
-          <div
-            class="bg-image hover-overlay ripple shadow-1-strong rounded"
-            data-ripple-color="light"
-          >
-            <img
-              src="../img/img4.jpg"
-              class="w-100"
-            />
-            <a href="#!" data-mdb-toggle="modal" data-mdb-target="#exampleModal3">
-              <div class="mask" style="background-color: rgba(251, 251, 251, 0.2);"></div>
-            </a>
-          </div>
-        </div>
 
-        <div class="col-lg-4 mb-4 mb-lg-0">
-            <div
-              class="bg-image hover-overlay ripple shadow-1-strong rounded"
-              data-ripple-color="light"
-            >
-              <img
-                src="../img/img4.jpg"
-                class="w-100"
-              />
-              <a href="#!" data-mdb-toggle="modal" data-mdb-target="#exampleModal3">
-                <div class="mask" style="background-color: rgba(251, 251, 251, 0.2);"></div>
-              </a>
-            </div>
-          </div>
-      </div>
     </section>
     <!-- Section: Images -->
   
@@ -211,57 +167,6 @@ $linhaEdit = $sqlEdita->fetch(PDO::FETCH_OBJ);
         </div>
       </div>
       <!-- Modal 2 -->
-      <div
-        class="modal fade"
-        id="exampleModal2"
-        tabindex="-1"
-        aria-labelledby="exampleModal2Label"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog modal-lg">
-          <div class="modal-content">
-            <div class="ratio ratio-16x9">
-              <iframe
-                src="https://www.youtube.com/embed/wTcNtgA6gHs"
-                title="YouTube video"
-                allowfullscreen
-              ></iframe>
-            </div>
-  
-            <div class="text-center py-3">
-              <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">
-                Close
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- Modal 3 -->
-      <div
-        class="modal fade"
-        id="exampleModal3"
-        tabindex="-1"
-        aria-labelledby="exampleModal3Label"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog modal-lg">
-          <div class="modal-content">
-            <div class="ratio ratio-16x9">
-              <iframe
-                src="https://www.youtube.com/embed/vlDzYIIOYmM"
-                title="YouTube video"
-                allowfullscreen
-              ></iframe>
-            </div>
-  
-            <div class="text-center py-3">
-              <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">
-                Close
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
     </section>
     <!-- Section: Modals -->
   </section>
